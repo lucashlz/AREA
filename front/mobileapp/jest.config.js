47e4c900f2b0264a142b6237f61be5c5c17fe258
Louis Chambon <louis.chambon@epitech.eu>
2023-09-18 13:05:06 +0200

    [ADD] test for jest test in mobileapp

--- front/mobileapp/jest.config.js
+++ front/mobileapp/jest.config.js
@@ -2,11 +2,10 @@ module.exports = {
     preset: "react-native",
     setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
     transform: {
-      "^.+\\.(js|jsx)$": "<rootDir>/front/mobileapp/node_modules/babel-jest",
-      "\\.(ts|tsx)$": "ts-jest",
+      "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
     },
     moduleFileExtensions: ["js", "jsx", "json", "node", "ts", "tsx"],
     moduleNameMapper: {
-      "^.+\\.(ttf|otf)$": "<rootDir>/front/mobileapp/__mocks__/fileMock.js",
+      "^.+\\.(ttf|otf)$": "<rootDir>/__mocks__/fileMock.js",
     },
   };
