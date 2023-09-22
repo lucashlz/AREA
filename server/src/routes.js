f84f796579541238548819df477b59e42c916635
Patteinh <patteinh@gmail.com>
2023-09-22 22:49:41 +0200

    [UPDATE] routes with profile request

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,6 @@
 const authRoutes = require("./routes/authRoutes");
 const oauthRoutes = require("./routes/oauthRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 const serviceRoutes = require("./routes/services");
 const swaggerUi = require("swagger-ui-express");
 const specs = require("./config/swagger");
@@ -8,5 +9,6 @@ module.exports = (app) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
   app.use("/auth", authRoutes);
   app.use("/auth", oauthRoutes);
+  app.use("/profile", profileRoutes);
   app.use("/services", serviceRoutes);
 };
