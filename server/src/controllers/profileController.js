2105dfb50252c9242b717ba95061795666fcea98
Patteinh <patteinh@gmail.com>
2023-09-24 03:52:31 +0200

    [UPDATE] profileController with new updateMail

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,11 +1,9 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const crypto = require("crypto");
-const { sendPasswordResetMail } = require("../utils/emailServices");
 
 exports.getUserProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select(
+    const user = await User.findById(req.user.id).select(
       "-password -confirmationToken"
     );
     if (!user) {
@@ -17,11 +15,32 @@ exports.getUserProfile = async (req, res) => {
   }
 };
 
+exports.updateEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findById(req.user.id);
+  try {
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.email = email;
+    await user.save();
+
+    res.json({
+      message: "Email updated successfully",
+      email: user.email,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 exports.updateUsername = async (req, res) => {
   const { username } = req.body;
 
+  const user = await User.findById(req.user.id);
   try {
-    const user = await User.findById(req.user._id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -42,7 +61,7 @@ exports.updatePassword = async (req, res) => {
   const { oldPassword, newPassword } = req.body;
 
   try {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user.id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -59,26 +78,6 @@ exports.updatePassword = async (req, res) => {
     await user.save();
 
     res.json({ message: "Password updated successfully" });
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-exports.requestPasswordReset = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const resetToken = crypto.randomBytes(20).toString("hex");
-    user.resetPasswordToken = resetToken;
-    user.resetPasswordExpires = Date.now() + 3600000;
-
-    await user.save();
-    sendPasswordResetMail(user.email, resetToken);
-
-    res.json({ message: "Reset email sent" });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
