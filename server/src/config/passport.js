4711135ea1705f586254ea093cf8392ae16f85b5
Patteinh <patteinh@gmail.com>
2023-09-24 21:36:17 +0200

    [UPDATE] hide connect route

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,7 +1,9 @@
 const passport = require("passport");
+const User = require('../models/user');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const GitHubStrategy = require("passport-github").Strategy;
+const SpotifyStrategy = require("passport-spotify").Strategy;
 const OAuth2Controller = require("../controllers/OAuth2Controller");
 const connectController = require("../controllers/connectController");
 
@@ -41,41 +43,5 @@ passport.use(
   )
 );
 
-passport.use(
-  "google-connect",
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/connect/google/callback",
-    },
-    connectController.connectGoogle
-  )
-);
-
-passport.use(
-  "facebook-connect",
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: "/connect/facebook/callback",
-      profileFields: ["id", "emails", "name"],
-    },
-    connectController.connectFacebook
-  )
-);
-
-passport.use(
-  "github-connect",
-  new GitHubStrategy(
-    {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/connect/github/callback",
-    },
-    connectController.connectGitHub
-  )
-);
 
 module.exports = passport;
--- server/src/controllers/connectController.js
+++ server/src/controllers/connectController.js
@@ -1,79 +1,105 @@
-const User = require("../models/user");
+// const User = require("../models/user");
 
-exports.connectGoogle = async (token, refreshToken, profile, done) => {
-  try {
-    let user = await User.findById(req.user.id);
-    if (user) {
-      if (!user.connectServices.google) {
-        user.connectServices.set("google", {
-          access_token: token,
-          refresh_token: refreshToken,
-          data: {
-            id: profile.id,
-            email: profile.emails[0].value,
-            name: profile.displayName,
-          },
-        });
-        await user.save();
-      }
-    } else {
-      done(new Error("User not found."));
-      return;
-    }
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-};
+// exports.connectGoogle = async (token, refreshToken, profile, done) => {
+//   try {
+//     let user = await User.findById(req.user._id);
+//     if (user) {
+//       if (!user.connectServices.google) {
+//         user.connectServices.set("google", {
+//           access_token: token,
+//           refresh_token: refreshToken,
+//           data: {
+//             id: profile.id,
+//             email: profile.emails[0].value,
+//             name: profile.displayName,
+//           },
+//         });
+//         await user.save();
+//       }
+//     } else {
+//       done(new Error("User not found."));
+//       return;
+//     }
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// };
 
-exports.connectFacebook = async (token, refreshToken, profile, done) => {
-  try {
-    let user = await User.findById(req.user.id);
-    if (user) {
-      if (!user.connectServices.facebook) {
-        user.connectServices.set("facebook", {
-          access_token: token,
-          refresh_token: refreshToken,
-          data: {
-            id: profile.id,
-            email: profile.emails ? profile.emails[0].value : null,
-            name: profile.displayName,
-          },
-        });
-        await user.save();
-      }
-    } else {
-      done(new Error("User not found."));
-      return;
-    }
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-};
+// exports.connectFacebook = async (token, refreshToken, profile, done) => {
+//   try {
+//     let user = await User.findById(req.user._id);
+//     if (user) {
+//       if (!user.connectServices.facebook) {
+//         user.connectServices.set("facebook", {
+//           access_token: token,
+//           refresh_token: refreshToken,
+//           data: {
+//             id: profile.id,
+//             email: profile.emails ? profile.emails[0].value : null,
+//             name: profile.displayName,
+//           },
+//         });
+//         await user.save();
+//       }
+//     } else {
+//       done(new Error("User not found."));
+//       return;
+//     }
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// };
 
-exports.connectGitHub = async (token, refreshToken, profile, done) => {
-  try {
-    let user = await User.findById(req.user.id);
-    if (user) {
-      if (!user.connectServices.github) {
-        user.connectServices.set("github", {
-          access_token: token,
-          refresh_token: refreshToken,
-          data: {
-            id: profile.id,
-            email: profile.emails ? profile.emails[0].value : null,
-            name: profile.displayName,
-          },
-        });
-        await user.save();
-      }
-    } else {
-      done(new Error("User not found."));
-      return;
-    }
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-};
+// exports.connectGitHub = async (token, refreshToken, profile, done) => {
+//   try {
+//     let user = await User.findById(req.user._id);
+//     if (user) {
+//       if (!user.connectServices.github) {
+//         user.connectServices.set("github", {
+//           access_token: token,
+//           refresh_token: refreshToken,
+//           data: {
+//             id: profile.id,
+//             email: profile.emails ? profile.emails[0].value : null,
+//             name: profile.displayName,
+//           },
+//         });
+//         await user.save();
+//       }
+//     } else {
+//       done(new Error("User not found."));
+//       return;
+//     }
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// };
+
+// exports.connectSpotify = async (token, refreshToken, profile, done) => {
+//   try {
+//     let user = await User.findById(req.user._id);
+//     if (user) {
+//       if (!user.connectServices.spotify) {
+//         user.connectServices.set("spotify", {
+//           access_token: token,
+//           refresh_token: refreshToken,
+//           data: {
+//             id: profile.id,
+//             email: profile.emails ? profile.emails[0].value : null,
+//             name: profile.displayName,
+//           },
+//         });
+//         await user.save();
+//       }
+//     } else {
+//       done(new Error("User not found."));
+//       return;
+//     }
+//     done(null, user);
+//   } catch (err) {
+//     done(err);
+//   }
+// };
