f86515fdfa5910d7903281430f6bc8bbe42c295b
Patteinh <patteinh@gmail.com>
2023-09-29 04:37:43 +0200

    [ADD] connectRoute with swagger

--- server/src/routes/connectRoutes.js
+++ server/src/routes/connectRoutes.js
@@ -124,4 +124,248 @@ router.get("/facebook", connectController.connectFacebookAccount);
  */
 router.get("/facebook/callback", connectController.connectFacebookCallback);
 
+/**
+ * @swagger
+ * /connect/github:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Connect to GitHub account
+ *     responses:
+ *       200:
+ *         description: Redirect to GitHub authentication page.
+ */
+router.get("/github", connectController.connectGitHubAccount);
+
+/**
+ * @swagger
+ * /connect/github/callback:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Callback from GitHub authentication
+ *     responses:
+ *       200:
+ *         description: GitHub connection successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Authentication failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error or failed to save user data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ */
+router.get("/github/callback", connectController.connectGitHubCallback);
+
+/**
+ * @swagger
+ * /connect/spotify:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Connect to Spotify account
+ *     responses:
+ *       200:
+ *         description: Redirect to Spotify authentication page.
+ */
+router.get("/spotify", connectController.connectSpotifyAccount);
+
+/**
+ * @swagger
+ * /connect/spotify/callback:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Callback from Spotify authentication
+ *     responses:
+ *       200:
+ *         description: Spotify connection successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Authentication failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error or failed to save user data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ */
+router.get("/spotify/callback", connectController.connectSpotifyCallback);
+
+/**
+ * @swagger
+ * /connect/discord:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Connect to Discord account
+ *     responses:
+ *       200:
+ *         description: Redirect to Discord authentication page.
+ */
+router.get("/discord", connectController.connectDiscordAccount);
+
+/**
+ * @swagger
+ * /connect/discord/callback:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Callback from Discord authentication
+ *     responses:
+ *       200:
+ *         description: Discord connection successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Authentication failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error or failed to save user data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ */
+router.get("/discord/callback", connectController.connectDiscordCallback);
+
+/**
+ * @swagger
+ * /connect/twitch:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Connect to Twitch account
+ *     responses:
+ *       200:
+ *         description: Redirect to Twitch authentication page.
+ */
+router.get("/twitch", connectController.connectTwitchAccount);
+
+/**
+ * @swagger
+ * /connect/twitch/callback:
+ *   get:
+ *     tags:
+ *       - connect
+ *     security:
+ *       - bearerAuth: []
+ *     description: Callback from Twitch authentication
+ *     responses:
+ *       200:
+ *         description: Twitch connection successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Authentication failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal server error or failed to save user data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ */
+router.get("/twitch/callback", connectController.connectTwitchCallback);
+
 module.exports = router;
