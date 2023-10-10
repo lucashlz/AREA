9c20ddeb99f821ae60ddefb837a4a4243a732740
Patteinh <patteinh@gmail.com>
2023-10-10 10:29:53 +0200

    [ADD] new connect controller with OAuthConstants return

--- server/src/controllers/connectController.js
+++ server/src/controllers/connectController.js
@@ -1,121 +1,228 @@
 const passport = require("passport");
+const { registerOAuthSession, verifyOAuthSession } = require("../utils/OAuthSessionUtils");
 require("../connection/googleStrategy");
-require("../connection/facebookStrategy");
 require("../connection/githubStrategy");
 require("../connection/discordStrategy");
 require("../connection/spotifyStrategy");
 require("../connection/twitchStrategy");
 
-exports.connectGoogleAccount = passport.authenticate("google-connect", {
-    scope: ["profile", "email", "https://www.googleapis.com/auth/gmail.send"],
-});
-
-exports.connectGoogleCallback = (req, res, next) => {
-    passport.authenticate("google-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
-        if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
-        }
-        res.json({
-            status: "success",
-            message: "Google connection successful.",
-        });
-    })(req, res, next);
+exports.getGoogleOAuthConstants = async (req, res) => {
+    const oAuthSessionId = await registerOAuthSession(req.user.id, "google");
+    if (!oAuthSessionId) {
+        return res.status(500).send("Failed to initiate OAuth session.");
+    }
+    return res.json({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        redirectUri: "http://localhost:8080/connect/google/callback",
+        scope: ["profile", "email"],
+        oAuthSessionId: oAuthSessionId,
+    });
 };
 
-exports.connectFacebookAccount = passport.authenticate("facebook-connect", {
-    scope: ["email"],
-});
+exports.googleCallback = async (req, res, next) => {
+    try {
+        const { state: oAuthSessionIdFromState } = req.query;
+        const { user } = await verifyOAuthSession(oAuthSessionIdFromState, "google");
 
-exports.connectFacebookCallback = (req, res, next) => {
-    passport.authenticate("facebook-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
         if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
+            return res
+                .status(400)
+                .json({ status: "error", message: "Invalid state or session expired" });
         }
-        res.json({
-            status: "success",
-            message: "Facebook connection successful.",
-        });
-    })(req, res, next);
+        req.user = user;
+        passport.authenticate("google-connect", (err, authenticatedUser) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Internal server error." });
+            }
+            if (!authenticatedUser) {
+                return res
+                    .status(401)
+                    .json({ status: "failed", message: "Authentication failed." });
+            }
+            res.json({
+                status: "success",
+                message: "Successfully connected with Google.",
+            });
+        })(req, res, next);
+    } catch (error) {
+        return res.status(500).json({ status: "error", message: "Unexpected error occurred." });
+    }
 };
 
-exports.connectGitHubAccount = passport.authenticate("github-connect", {
-    scope: ["user:email"],
-});
-
-exports.connectGitHubCallback = (req, res, next) => {
-    passport.authenticate("github-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
-        if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
-        }
-        res.json({
-            status: "success",
-            message: "GitHub connection successful.",
-        });
-    })(req, res, next);
+exports.getGithubOAuthConstants = async (req, res) => {
+    const oAuthSessionId = await registerOAuthSession(req.user.id, "github");
+    if (!oAuthSessionId) {
+        return res.status(500).send("Failed to initiate OAuth session.");
+    }
+    return res.json({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        redirectUri: "http://localhost:8080/connect/github/callback",
+        scope: ["user:email"],
+        oAuthSessionId: oAuthSessionId,
+    });
 };
 
-exports.connectSpotifyAccount = passport.authenticate("spotify-connect", {
-    scope: ["user-read-email", "user-read-private"],
-});
-
-exports.connectSpotifyCallback = (req, res, next) => {
-    passport.authenticate("spotify-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
+exports.githubCallback = async (req, res, next) => {
+    try {
+        const { state: oAuthSessionIdFromState } = req.query;
+        const { user } = await verifyOAuthSession(oAuthSessionIdFromState, "github");
         if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
+            return res
+                .status(400)
+                .json({ status: "error", message: "Invalid state or session expired" });
         }
-        res.json({
-            status: "success",
-            message: "Spotify connection successful.",
-        });
-    })(req, res, next);
+        req.user = user;
+        passport.authenticate("github-connect", (err, authenticatedUser) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Internal server error." });
+            }
+            if (!authenticatedUser) {
+                return res
+                    .status(401)
+                    .json({ status: "failed", message: "Authentication failed." });
+            }
+            res.json({
+                status: "success",
+                message: "Successfully connected with Github.",
+            });
+        })(req, res, next);
+    } catch (error) {
+        return res.status(500).json({ status: "error", message: "Unexpected error occurred." });
+    }
 };
 
-exports.connectDiscordAccount = passport.authenticate("discord-connect", {
-    scope: ["identify", "email"],
-});
-
-exports.connectDiscordCallback = (req, res, next) => {
-    passport.authenticate("discord-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
-        if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
-        }
-        res.json({
-            status: "success",
-            message: "Discord connection successful.",
-        });
-    })(req, res, next);
+exports.getSpotifyOAuthConstants = async (req, res) => {
+    const oAuthSessionId = await registerOAuthSession(req.user.id, "spotify");
+    if (!oAuthSessionId) {
+        return res.status(500).send("Failed to initiate OAuth session.");
+    }
+    return res.json({
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        redirectUri: "http://localhost:8080/connect/spotify/callback",
+        scopes: [
+            "user-library-read",
+            "user-read-recently-played",
+            "playlist-read-private",
+            "playlist-modify-private",
+            "playlist-modify-public",
+            "user-library-modify",
+            "user-follow-modify",
+        ],
+        oAuthSessionId: oAuthSessionId,
+    });
 };
 
-exports.connectTwitchAccount = passport.authenticate("twitch-connect", {
-    scope: ["user_read"],
-});
+exports.spotifyCallback = async (req, res, next) => {
+    try {
+        const { state: oAuthSessionIdFromState } = req.query;
+        const { user } = await verifyOAuthSession(oAuthSessionIdFromState, "spotify");
 
-exports.connectTwitchCallback = (req, res, next) => {
-    passport.authenticate("twitch-connect", (err, user) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: "Internal server error." });
-        }
         if (!user) {
-            return res.status(401).json({ status: "failed", message: "Authentication failed." });
+            return res
+                .status(400)
+                .json({ status: "error", message: "Invalid state or session expired" });
         }
-        res.json({
-            status: "success",
-            message: "Twitch connection successful.",
-        });
-    })(req, res, next);
+        req.user = user;
+        passport.authenticate("spotify-connect", (err, authenticatedUser) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Internal server error." });
+            }
+            if (!authenticatedUser) {
+                return res
+                    .status(401)
+                    .json({ status: "failed", message: "Authentication failed." });
+            }
+            res.json({
+                status: "success",
+                message: "Successfully connected with Spotify.",
+            });
+        })(req, res, next);
+    } catch (error) {
+        return res.status(500).json({ status: "error", message: "Unexpected error occurred." });
+    }
+};
+
+exports.getDiscordOAuthConstants = async (req, res) => {
+    const oAuthSessionId = await registerOAuthSession(req.user.id, "discord");
+    if (!oAuthSessionId) {
+        return res.status(500).send("Failed to initiate OAuth session.");
+    }
+    return res.json({
+        clientId: process.env.DISCORD_CLIENT_ID,
+        redirectUri: "http://localhost:8080/connect/discord/callback",
+        scope: ["identify", "email"],
+        oAuthSessionId: oAuthSessionId,
+    });
+};
+
+exports.discordCallback = async (req, res, next) => {
+    try {
+        const { state: oAuthSessionIdFromState } = req.query;
+        const { user } = await verifyOAuthSession(oAuthSessionIdFromState, "discord");
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ status: "error", message: "Invalid state or session expired" });
+        }
+        req.user = user;
+        passport.authenticate("discord-connect", (err, authenticatedUser) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Internal server error." });
+            }
+            if (!authenticatedUser) {
+                return res
+                    .status(401)
+                    .json({ status: "failed", message: "Authentication failed." });
+            }
+            res.json({
+                status: "success",
+                message: "Successfully connected with Discord.",
+            });
+        })(req, res, next);
+    } catch (error) {
+        return res.status(500).json({ status: "error", message: "Unexpected error occurred." });
+    }
+};
+
+exports.getTwitchOAuthConstants = async (req, res) => {
+    const oAuthSessionId = await registerOAuthSession(req.user.id, "twitch");
+    if (!oAuthSessionId) {
+        return res.status(500).send("Failed to initiate OAuth session.");
+    }
+    return res.json({
+        clientId: process.env.TWITCH_CLIENT_ID,
+        redirectUri: "http://localhost:8080/connect/twitch/callback",
+        scope: ["user_read"],
+        oAuthSessionId: oAuthSessionId,
+    });
+};
+
+exports.twitchCallback = async (req, res, next) => {
+    try {
+        const { state: oAuthSessionIdFromState } = req.query;
+        const { user } = await verifyOAuthSession(oAuthSessionIdFromState, "twitch");
+        if (!user) {
+            return res
+                .status(400)
+                .json({ status: "error", message: "Invalid state or session expired" });
+        }
+        req.user = user;
+        passport.authenticate("twitch-connect", (err, authenticatedUser) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Internal server error." });
+            }
+            if (!authenticatedUser) {
+                return res
+                    .status(401)
+                    .json({ status: "failed", message: "Authentication failed." });
+            }
+            res.json({
+                status: "success",
+                message: "Successfully connected with Twitch.",
+            });
+        })(req, res, next);
+    } catch (error) {
+        return res.status(500).json({ status: "error", message: "Unexpected error occurred." });
+    }
 };
