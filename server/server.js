3f8ad303bd77b054ccbfc3bd169831853cdccc0c
Patteinh <patteinh@gmail.com>
2023-10-10 10:27:42 +0200

    [FIX] docker compose and server.js start with interval

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const { setupAppMiddleware } = require("./src/middleware/middleware");
+const { checkAndReact } = require("./src/core/areaTrigger");
+const { refreshTokensForAllUsers } = require("./src/utils/tokenUtils");
 const setupRoutes = require("./src/routes.js");
 const connectDB = require("./src/config/dbConfig");
-const cors = require("cors");
-const { checkAndReact } = require("./src/core/areaTrigger");
 require("dotenv").config();
 
 const app = express();
-
-app.use(cors());
-
-app.all("/*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
 setupAppMiddleware(app);
 connectDB();
 setupRoutes(app);
@@ -23,7 +14,28 @@ setupRoutes(app);
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});
+    const REFRESH_INTERVAL = 60 * 60 * 1000;
 
-const EVALUATION_INTERVAL = 2000;
-setInterval(checkAndReact, EVALUATION_INTERVAL);
+    function recursiveRefresh() {
+        refreshTokensForAllUsers();
+
+        let startTime = Date.now();
+        let endTime = startTime + REFRESH_INTERVAL;
+
+        function printTimeRemaining() {
+            let now = Date.now();
+            let timeRemaining = Math.round((endTime - now) / 1000);
+            console.log(`Time until next token refresh: ${timeRemaining} seconds`);
+
+            if (now < endTime) {
+                setTimeout(printTimeRemaining, 60000);
+            } else {
+                recursiveRefresh();
+            }
+        }
+        printTimeRemaining();
+    }
+    const EVALUATION_INTERVAL = 2000;
+    setInterval(checkAndReact, EVALUATION_INTERVAL);
+    recursiveRefresh();
+});
