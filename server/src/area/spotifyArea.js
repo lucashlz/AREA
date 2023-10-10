14c55aac475cd3a66e7d41405ca2f21e0b9b287b
Patteinh <patteinh@gmail.com>
2023-10-10 10:42:58 +0200

    [UPDATE] spotify area

--- server/src/area/spotifyArea.js
+++ server/src/area/spotifyArea.js
@@ -7,42 +7,76 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:8080/connect/spotify/callback",
 });
 
-async function getTopTracks(userId) {
+async function setSpotifyToken(userId) {
     const user = await User.findById(userId);
-    spotifyApi.setAccessToken(user.spotifyToken);
-
-    const response = await spotifyApi.getMyTopTracks();
-    return response.body.items;
+    if (user && user.connectServices && user.connectServices.spotify) {
+        spotifyApi.setAccessToken(user.connectServices.spotify.access_token);
+    } else {
+        throw new Error("Failed to set Spotify token for user.");
+    }
 }
 
-async function getPlaylistCount(userId) {
-    const user = await User.findById(userId);
-    spotifyApi.setAccessToken(user.spotifyToken);
-
-    const response = await spotifyApi.getUserPlaylists(user.spotifyUsername);
-    return response.body.items.length;
+async function newSavedTrack(userId) {
+    await setSpotifyToken(userId);
+    const savedTracks = await spotifyApi.getMySavedTracks({ limit: 1 });
+    return savedTracks.body.items.length > 0 ? savedTracks.body.items[0] : null;
 }
 
-exports.newTopTrackTrigger = async (userId, lastTopTrackId) => {
-    const topTracks = await getTopTracks(userId);
-    return topTracks[0].id !== lastTopTrackId;
-};
+async function newSavedAlbum(userId) {
+    await setSpotifyToken(userId);
+    const savedAlbums = await spotifyApi.getMySavedAlbums({ limit: 1 });
+    return savedAlbums.body.items.length > 0 ? savedAlbums.body.items[0] : null;
+}
 
-exports.newPlaylistTrigger = async (userId, lastCount) => {
-    const currentCount = await getPlaylistCount(userId);
-    return currentCount > lastCount;
-};
+async function newRecentlyPlayedTrack(userId) {
+    await setSpotifyToken(userId);
+    const recentlyPlayedTracks = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 1 });
+    return recentlyPlayedTracks.body.items.length > 0
+        ? recentlyPlayedTracks.body.items[0].track
+        : null;
+}
 
-exports.addToPlaylist = async (userId, trackId, playlistId) => {
-    const user = await User.findById(userId);
-    spotifyApi.setAccessToken(user.spotifyToken);
+async function newTrackAddedToPlaylist(userId, playlistName) {
+    await setSpotifyToken(userId);
+    const playlists = await spotifyApi.getUserPlaylists();
+    const targetPlaylist = playlists.body.items.find((playlist) => playlist.name === playlistName);
+    if (!targetPlaylist) {
+        throw new Error("Specified playlist not found");
+    }
+    const playlistId = targetPlaylist.id;
+    const tracksInPlaylist = await spotifyApi.getPlaylistTracks(playlistId);
+    return tracksInPlaylist.body.items.length > 0 ? tracksInPlaylist.body.items[0].track : null;
+}
 
-    return spotifyApi.addTracksToPlaylist(playlistId, [`spotify:track:${trackId}`]);
-};
+async function followPlaylist(userId, playlistId) {
+    await setSpotifyToken(userId);
+    return spotifyApi.followPlaylist(playlistId);
+}
 
-exports.createPlaylist = async (userId, playlistName) => {
-    const user = await User.findById(userId);
-    spotifyApi.setAccessToken(user.spotifyToken);
+async function saveTrack(userId, trackName) {
+    await setSpotifyToken(userId);
+    const searchResult = await spotifyApi.searchTracks(trackName);
+    const trackId = searchResult.body.tracks.items[0].id;
+    return spotifyApi.addToMySavedTracks([trackId]);
+}
 
-    return spotifyApi.createPlaylist(user.spotifyUsername, playlistName, { public: false });
+async function addToPlaylistById(userId, playlistName, trackId) {
+    await setSpotifyToken(userId);
+    const playlists = await spotifyApi.getUserPlaylists();
+    let targetPlaylist = playlists.body.items.find((playlist) => playlist.name === playlistName);
+    if (!targetPlaylist) {
+        const createdPlaylist = await spotifyApi.createPlaylist(playlistName, { public: false });
+        targetPlaylist = createdPlaylist.body;
+    }
+    return spotifyApi.addTracksToPlaylist(targetPlaylist.id, [`spotify:track:${trackId}`]);
+}
+
+module.exports = {
+    newSavedTrack,
+    newSavedAlbum,
+    newRecentlyPlayedTrack,
+    newTrackAddedToPlaylist,
+    followPlaylist,
+    addToPlaylistById,
+    saveTrack,
 };
