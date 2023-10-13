492655be61d305eba6d18aaf2d9d935a5853760a
Patteinh <patteinh@gmail.com>
2023-10-13 21:42:59 +0200

    [ADD] disconnect service

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -16,3 +16,32 @@ exports.deleteLoggedInUser = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+exports.disconnectService = async (req, res) => {
+    try {
+        const serviceName = req.params.service_name;
+
+        if (!serviceName) {
+            return res.status(400).json({ message: "Service name not provided." });
+        }
+        const user = req.user;
+        if (user.connectServices.has(serviceName)) {
+            user.connectServices.delete(serviceName);
+            await user.save();
+            await Area.updateMany(
+                {
+                    userId: user._id,
+                    $or: [{ "trigger.service": serviceName }, { "actions.service": serviceName }],
+                },
+                { isActive: false }
+            );
+            res.status(200).json({
+                message: `Successfully disconnected from ${serviceName} and related areas have been deactivated.`,
+            });
+        } else {
+            res.status(400).json({ message: `User is not connected to ${serviceName}.` });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error disconnecting the service.", error });
+    }
+};
