552e4a85eacf5b05ff1814909fe5b14987128bd4
Patteinh <patteinh@gmail.com>
2023-10-23 03:31:02 +0200

    [UPDATE] email tools with generated mail template

--- server/src/utils/emailUtils.js
+++ server/src/utils/emailUtils.js
@@ -9,15 +9,38 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const iftttLogoUrl = "https://images.ctfassets.net/mrsnpomeucef/4FcLBcGJLV3Tru34MImgA3/3a4c836ebf29d4363c4a172e62ee5e80/Wordmark_on_gray.png";
+
+const generateMailTemplate = (bodyContent) => {
+    return `
+    <div style="font-family: 'Archivo', Arial, sans-serif; padding: 40px; font-size: 16px; background-color: #f5f5f5;">
+        <div style="background-color: #ffffff; padding: 30px; border-radius: 10px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+            <div style="text-align: center; margin-bottom: 30px;">
+                <img src="${iftttLogoUrl}" alt="IFTTT Logo" style="max-width: 250px; border: none;">
+            </div>
+            ${bodyContent}
+        </div>
+        <div style="padding-top: 30px; text-align: center;">
+            <p style="color: #999999;">&copy; ${new Date().getFullYear()} AREA. All rights reserved.</p>
+        </div>
+    </div>
+    `;
+};
+
 const sendConfirmationMail = async (email, confirmationToken) => {
+  const bodyContent = `
+    <p>To confirm your email, please click the link below:</p>
+    <p><a href="http://localhost:8081/auth/confirm/${confirmationToken}" style="color: #4CAF50;">Confirm Your Email</a></p>
+  `;
+
   const mailOptions = {
-    from: `"AREA" <${process.env.EMAIL}>`,
+    from: `"IFTTT" <${process.env.EMAIL}>`,
     to: email,
-    subject: "[AREA] Please confirm your email",
-    text: `Click this link to confirm your email: http://localhost:8081/auth/confirm/${confirmationToken}`,
+    subject: "[IFTTT] Please confirm your email",
+    html: generateMailTemplate(bodyContent)
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
     } else {
@@ -27,14 +50,19 @@ const sendConfirmationMail = async (email, confirmationToken) => {
 };
 
 const sendEmailChangeConfirmationMail = async (email, emailChangeToken) => {
+  const bodyContent = `
+    <p>To confirm your email change, please click the link below:</p>
+    <p><a href="http://localhost:8081/auth/confirm-email-change/${emailChangeToken}" style="color: #4CAF50;">Confirm Email Change</a></p>
+  `;
+
   const mailOptions = {
-    from: `"AREA" <${process.env.EMAIL}>`,
+    from: `"IFTTT" <${process.env.EMAIL}>`,
     to: email,
-    subject: "[AREA] Please confirm your email change",
-    text: `Click this link to confirm your email change: http://localhost:8081/auth/confirm-email-change/${emailChangeToken}`,
+    subject: "[IFTTT] Please confirm your email change",
+    html: generateMailTemplate(bodyContent)
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
     } else {
@@ -44,17 +72,21 @@ const sendEmailChangeConfirmationMail = async (email, emailChangeToken) => {
 };
 
 const sendPasswordResetMail = async (email, resetToken) => {
+  const bodyContent = `
+    <p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
+    <p>Please click on the link below to complete the process within one hour of receiving it:</p>
+    <p><a href="http://localhost:8080/reset/password/${resetToken}" style="color: #4CAF50;">Reset Your Password</a></p>
+    <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
+  `;
+
   const mailOptions = {
     from: process.env.EMAIL,
     to: email,
-    subject: "[AREA] Password Reset Request",
-    text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
-    Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
-    http://localhost:8080/reset/password/${resetToken}\n\n
-    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    subject: "[IFTTT] Password Reset Request",
+    html: generateMailTemplate(bodyContent)
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
     } else {
