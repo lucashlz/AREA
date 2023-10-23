4998329a8029146d4ec1dd1a37fda4c31d656b2f
Patteinh <patteinh@gmail.com>
2023-10-23 03:26:22 +0200

    [UPDATE] Youtube triggers/actions to valis trigger

--- server/src/area/youtubeArea.js
+++ server/src/area/youtubeArea.js
@@ -3,50 +3,78 @@ const User = require("../models/userModels");
 
 async function setYouTubeToken(userId) {
     const user = await User.findById(userId);
-    if (user && user.connectServices && user.connectServices.youtube) {
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_CLIENT_ID,
-            process.env.GOOGLE_CLIENT_SECRET,
-            "http://localhost:8080/connect/google/callback"
-        );
-
-        oauth2Client.setCredentials({ access_token: user.connectServices.youtube.access_token });
-        return google.youtube({ version: "v3", auth: oauth2Client });
-    } else {
-        throw new Error("Failed to set YouTube token for user.");
+    if (!user) {
+        console.error("User not found:", userId);
+        return null;
     }
+    const youtubeService = user.connectServices.get("youtube");
+    if (!youtubeService) {
+        console.error("Youtube service not available for user:", user._id);
+        return null;
+    }
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        "http://localhost:8080/connect/google/callback"
+    );
+    oauth2Client.setCredentials({ access_token: youtubeService.access_token });
+    return google.youtube({ version: "v3", auth: oauth2Client });
 }
 
-async function newLikedVideo(userId) {
-    const youtube = await setYouTubeToken(userId);
-    const response = await youtube.activities.list({
-        part: "snippet",
+async function processTriggerData(areaEntry, key, value) {
+    if (!areaEntry.trigger.data) {
+        areaEntry.trigger.data = { key, value };
+        await areaEntry.save();
+        return false;
+    } else if (areaEntry.trigger.data.value !== value) {
+        areaEntry.trigger.data = { key, value };
+        await areaEntry.save();
+        return true;
+    }
+    return false;
+}
+
+async function newLikedVideo(areaEntry) {
+    const youtube = await setYouTubeToken(areaEntry.userId);
+    const channelResponse = await youtube.channels.list({
+        part: "contentDetails",
         mine: true,
-        maxResults: 1,
-        type: "like",
+        maxResults: 1
     });
-    return response.data.items.length > 0 ? response.data.items[0] : null;
+    const likedVideosPlaylistId = channelResponse.data.items[0].contentDetails.relatedPlaylists.likes;
+    const response = await youtube.playlistItems.list({
+        part: "snippet",
+        playlistId: likedVideosPlaylistId,
+        maxResults: 1
+    });
+    const recentLike = response.data.items.length > 0 ? response.data.items[0].snippet : null;
+    if (!recentLike) return false;
+    return await processTriggerData(areaEntry, "videoId", recentLike.resourceId.videoId);
 }
 
-async function newVideoByChannel(userId, channelId) {
-    const youtube = await setYouTubeToken(userId);
+async function newVideoByChannel(areaEntry, channelId) {
+    const youtube = await setYouTubeToken(areaEntry.userId);
     const response = await youtube.search.list({
         part: "snippet",
         channelId: channelId,
         maxResults: 1,
         order: "date",
     });
-    return response.data.items.length > 0 ? response.data.items[0] : null;
+    const recentVideo = response.data.items.length > 0 ? response.data.items[0] : null;
+    if (!recentVideo) return false;
+    return await processTriggerData(areaEntry, "channelVideoId", recentVideo.id);
 }
 
-async function newSubscription(userId, channelId) {
-    const youtube = await setYouTubeToken(userId);
+async function newSubscription(areaEntry) {
+    const youtube = await setYouTubeToken(areaEntry.userId);
     const response = await youtube.subscriptions.list({
         part: "snippet",
-        channelId: channelId,
+        mine: true,
         maxResults: 1,
     });
-    return response.data.items.length > 0 ? response.data.items[0] : null;
+    const recentSubscription = response.data.items.length > 0 ? response.data.items[0] : null;
+    if (!recentSubscription) return false;
+    return await processTriggerData(areaEntry, "subscriptionId", recentSubscription.id);
 }
 
 async function likeVideo(userId, videoId) {
