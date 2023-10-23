d21fccb369967c92d3f8c931a45e5dfe7e26e1f2
AdeledePremonville <adele.de-premonville@epitech.eu>
2023-10-23 14:18:53 +0200

    [ADD] search bar algo

--- front/webapp/src/components/SearchBar.tsx
+++ front/webapp/src/components/SearchBar.tsx
@@ -1,20 +1,29 @@
 import { useEffect } from "react";
-import Input, { InputProps } from "./Input";
-import { InterfaceType } from "typescript";
+import Input from "./Input";
 
 type SearchBarProps = {
-    style?: React.CSSProperties,
-    setSearchInput: (e :string) => void,
-    searchInput: string,
-    items: any[],
-    setItems: (item: any[]) => void,
-    name: string
-}
+  style?: React.CSSProperties;
+  setSearchInput: (e: string) => void;
+  searchInput: string;
+  items: any[];
+  setItems: (item: any[]) => void;
+  name: string;
+};
 
 export default function SearchBar(props: SearchBarProps) {
-    useEffect(() => {
-        props.setItems([...props.items].sort((a, b) => {console.log("name a : ", a); return (a[props.name].localeCompare(b[props.name]))}))
-    }, [props.searchInput])
+  useEffect(() => {
+    const sortedItems = [...props.items].sort((a, b) => {
+      const distanceA = a[props.name]
+        .toLowerCase()
+        .indexOf(props.searchInput.toLowerCase());
+      const distanceB = b[props.name]
+        .toLowerCase()
+        .indexOf(props.searchInput.toLowerCase());
+      return distanceB - distanceA;
+    });
+
+    props.setItems(sortedItems);
+  }, [props.searchInput]);
   return (
     <div className="applets-searchbar-holder" style={props.style}>
       <Input
