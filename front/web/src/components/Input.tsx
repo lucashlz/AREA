136f0078d1f6310bfdc9bd6d8772a765a148708b
AdeledePremonville <adele.de-premonville@epitech.eu>
2023-10-30 02:38:30 +0100

    [ADD] responsivelogin page + navbar

--- front/web/src/components/Input.tsx
+++ front/web/src/components/Input.tsx
@@ -1,30 +1,27 @@
-import './Input.css';
+import "./Input.css";
 
 export type InputProps = {
-  placeholder: string,
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
-  value: string,
-  type: string,
-  icon?: string,
-  required?: boolean,
-}
+  placeholder: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  type: string;
+  icon?: string;
+  required?: boolean;
+};
 
 export default function Input(props: InputProps) {
   return (
-    <div className='field-container'>
-      <div className='input-icon-container'>
-        {props.icon && <img src={props.icon} alt="Icon" className='input-icon' />}
-        <input
-          type={props.type}
-          className='input'
-          style={props.icon ? {paddingLeft: '60px'} : {}}
-          placeholder={props.placeholder}
-          value={props.value}
-          required={props.required === undefined ? false : props.required}
-          onChange={props.onChange}
-        />
-      </div>
+    <div className="input-icon-container">
+      {props.icon && <img src={props.icon} alt="Icon" className="input-icon" />}
+      <input
+        type={props.type}
+        className="input"
+        style={props.icon ? { paddingLeft: "60px" } : {}}
+        placeholder={props.placeholder}
+        value={props.value}
+        required={props.required === undefined ? false : props.required}
+        onChange={props.onChange}
+      />
     </div>
-  )
+  );
 }
-
--- front/web/src/components/pages/Login.tsx
+++ front/web/src/components/pages/Login.tsx
@@ -2,15 +2,13 @@ import React, { useState, useContext } from 'react';
 import { Navigate} from 'react-router-dom';
 import { UserContext, IUserContext } from '../../context/userContext';
 import './Login.css';
-import Title from '../Title';
 import Input from '../Input';
+import Title from '../Title';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [oauth, setOauth] = useState(false);
-  const [url, setUrl] = useState('');
   const [passwordPage, setPasswordPage] = useState(false);
   const [message, setMessage] = useState<string | null>(null);
 
@@ -18,7 +16,6 @@ const Login: React.FC = () => {
 
 
   const handleSubmit = async (e: React.FormEvent) => {
-    console.log("SUBMIT");
     e.preventDefault();
     try {
       const response = await signIn(email, password);
@@ -38,30 +35,19 @@ const Login: React.FC = () => {
     return <Navigate to="/applets" replace />;
   }
 
-  if (oauth && url.length > 0) {
-    return <Navigate to={`http://localhost:8080/auth/${url}`} />;
-  }
-
-  const getOAuth = async (url: String) => {
-    try {
-      console.log("Clicked!!!!!\n")
-      return <Navigate to={`http://localhost:8080/auth/${url}`} />;
-    } catch (error) {
-      console.error(`Error signing up with ${url}:`, error);
-    }
-  }
-
   return (
     <div className="login-container">
-      <form>
-        <div className='login'>Log in</div>
+      <form className='login-form'>
+        <div className='login-title'>
+        <Title title='Log in' />
+        </div>
         <Input onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' value={email}/>
         <Input onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' value={password}/>
         {message && <div className="login-response-message">{message}</div>}
         {error && <p>{error}</p>}
         <div className='underline-text' style={{fontSize: 20}} onClick={() => setPasswordPage(true)} >Forgot your password ?</div>
         <button type="button" className="btn btn--primary-inverted btn--large" onClick={handleSubmit}>Log in</button>
-        <div style={{fontWeight: 500, fontSize: 25, marginTop: '10%'}} >Continue with <a href="http://localhost:8080/auth/google" className='underline-text'>Google</a></div>
+        <div className='login-auth' >Continue with <a href="http://localhost:8080/auth/google" className='underline-text'>Google</a></div>
       </form>
     </div>
   );
