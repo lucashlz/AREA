963f98e0ec49529a1494c649c960a3ffc0cd02f4
Patteinh <patteinh@gmail.com>
2023-10-31 05:37:34 +0100

    [ADD] checkparameters for Dropbox

--- server/src/utils/tokenUtils.js
+++ server/src/utils/tokenUtils.js
@@ -15,43 +15,31 @@ async function refreshTokensForAllUsers() {
 
 function isTokenExpired(user, serviceName) {
     console.log(`Checking token expiration for user: ${user._id} and service: ${serviceName}`);
-
     const serviceData = user.connectServices.get(serviceName);
     if (!serviceData) {
-        console.log(
-            `No service data found for user: ${user._id} and service: ${serviceName}.`
-        );
+        console.log(`No service data found for user: ${user._id} and service: ${serviceName}.`);
         return false;
     }
     const tokenIssuedAt = serviceData.tokenIssuedAt;
     if (!tokenIssuedAt) {
-        console.log(
-            `No tokenIssuedAt found for user: ${user._id} and service: ${serviceName}. Assuming token is expired.`
-        );
+        console.log(`No tokenIssuedAt found for user: ${user._id} and service: ${serviceName}. Assuming token is expired.`);
         return true;
     }
-
     const expiresIn = serviceData.expires_in;
     if (!expiresIn) {
-        console.log(
-            `No expiresIn found for user: ${user._id} and service: ${serviceName}. Assuming token is expired.`
-        );
+        console.log(`No expiresIn found for user: ${user._id} and service: ${serviceName}. Assuming token is expired.`);
         return true;
     }
-    const currentAdjustedTime = Date.now()
-
+    const currentAdjustedTime = Date.now();
     console.log(`Current time (ms since epoch): ${currentAdjustedTime}`);
     console.log(`tokenIssuedAt (ms since epoch): ${tokenIssuedAt}`);
-
     const timeElapsed = currentAdjustedTime - tokenIssuedAt;
     const isExpired = timeElapsed >= expiresIn;
-
     console.log(
         `For user: ${user._id} and service: ${serviceName}, token issued at: ${new Date(
             tokenIssuedAt
         ).toISOString()}, expiresIn: ${expiresIn}ms, time elapsed: ${timeElapsed}ms, isExpired: ${isExpired}`
     );
-
     return isExpired;
 }
 
@@ -76,14 +64,13 @@ async function refreshServiceTokenForUser(user, serviceName, refreshTokenFunctio
 async function refreshAllServiceTokens(userId) {
     const user = await User.findById(userId);
     if (!user) throw new Error("User not found.");
-
     const services = [
         { name: "spotify", refreshFunction: refreshSpotifyToken },
         { name: "youtube", refreshFunction: refreshGoogleToken },
         { name: "gmail", refreshFunction: refreshGoogleToken },
         { name: "twitch", refreshFunction: refreshTwitchToken },
+        { name: "dropbox", refreshFunction: refreshDropboxToken },
     ];
-
     for (let service of services) {
         if (isTokenExpired(user, service.name)) {
             await refreshServiceTokenForUser(user, service.name, service.refreshFunction);
@@ -98,17 +85,11 @@ async function refreshSpotifyToken(refreshToken) {
         client_id: process.env.SPOTIFY_CLIENT_ID,
         client_secret: process.env.SPOTIFY_CLIENT_SECRET,
     };
-
-    const response = await axios.post(
-        "https://accounts.spotify.com/api/token",
-        qs.stringify(data),
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        }
-    );
-
+    const response = await axios.post("https://accounts.spotify.com/api/token", qs.stringify(data), {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    });
     return response.data.access_token;
 }
 
@@ -130,6 +111,25 @@ async function refreshTwitchToken(refreshToken) {
         client_secret: process.env.TWITCH_CLIENT_SECRET,
     });
     return response.data.access_token;
+}
+
+async function refreshDropboxToken(refreshToken) {
+    const data = {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+        client_id: process.env.DROPBOX_CLIENT_ID,
+        client_secret: process.env.DROPBOX_CLIENT_SECRET,
+    };
+    const response = await axios.post("https://api.dropboxapi.com/oauth2/token", qs.stringify(data), {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    });
+    if (response.data && response.data.access_token) {
+        return response.data.access_token;
+    } else {
+        throw new Error("Failed to refresh Dropbox token");
+    }
 }
 
 module.exports = {
