c8b2a1c59e48c7aca2b958b45d5e3eaa0d7f13ff
Patteinh <patteinh@gmail.com>
2023-11-05 05:41:05 +0100

    [UPDATE] profileController

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,87 +1,41 @@
 const User = require("../models/userModels");
-const { hasAuthService, updateUserEmail, updateUserPassword } = require("../utils/profileUtils");
+const { handleUserProfile } = require("../utils/profile/userProfileHandlers");
+const { handleProfileUpdate } = require("../utils/profile/userProfileHandlers");
+const { generateUpdateMessage } = require("../utils/profile/messageGenerators");
+const { USER_NOT_FOUND_MSG, GOOGLE_AUTH_MSG } = require("../utils/profile/userProfileHandlers");
+
+const SERVER_ERROR_MSG = "Server error";
 
 exports.getUserProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select("-_id username email connectServices");
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        const serviceNames = Array.from(user.connectServices.keys());
-        res.json({
-            ...user._doc,
-            connectServices: serviceNames,
-        });
+        const profile = await handleUserProfile(req.user.id);
+        res.json(profile);
     } catch (err) {
-        res.status(500).json({ message: "Server error" });
+        res.status(err.message === USER_NOT_FOUND_MSG ? 404 : 500).json({ message: err.message || SERVER_ERROR_MSG });
     }
 };
 
 exports.updateProfile = async (req, res) => {
+    const { email, username, oldPassword, newPassword } = req.body;
+
     try {
-        const { email, username, oldPassword, newPassword } = req.body;
         const user = await User.findById(req.user.id);
-        const updatedFields = [];
-        if (!user) return res.status(404).json({ message: "User not found" });
-        const hasGoogleAuth = hasAuthService(user, "google");
+        if (!user) throw new Error(USER_NOT_FOUND_MSG);
 
-        if (username && username !== user.username) {
-            user.username = username;
-            updatedFields.push("Username");
-        }
-
-        if (email) {
-            if (hasGoogleAuth) {
-                return res.status(403).json({
-                    message:
-                        "You've previously logged in using an external service. You cannot change your email.",
-                });
-            }
-            if (email !== user.email) {
-                await updateUserEmail(user, email);
-                updatedFields.push("Email");
-            }
-        }
-
-        if (oldPassword && newPassword) {
-            if (oldPassword === newPassword) {
-                return res.status(400).json({ message: "Same old and new password provided." });
-            }
-            if (hasGoogleAuth) {
-                return res.status(403).json({
-                    message:
-                        "You've previously logged in using an external service. You cannot change your password.",
-                });
-            }
-            await updateUserPassword(user, oldPassword, newPassword);
-            updatedFields.push("Password");
-        }
-        await user.save();
-        const messageComponents = [];
-        const updateSuccessFields = updatedFields.filter(field => ['Username', 'Password'].includes(field));
-        if (updateSuccessFields.length) {
-            messageComponents.push(`${updateSuccessFields.join(", ")} updated successfully.`);
-        }
-
-        if (updatedFields.includes("Email")) {
-            messageComponents.push("Please check your email to confirm the new email adress.");
-        } else if (!updatedFields.length) {
-            messageComponents.push("No fields were changed.");
-        }
-        const message = messageComponents.join(" ");
+        const updatedFields = await handleProfileUpdate(user, email, username, oldPassword, newPassword);
+        const message = generateUpdateMessage(updatedFields);
         res.json({ message });
     } catch (err) {
         console.error(err);
-        if (
-            [
-                "Invalid email format",
-                "Email is already in use",
-                "Old password is incorrect",
-                "Same old and new password provided."
-            ].includes(err.message)
-        ) {
-            return res.status(400).json({ message: err.message });
-        }
-        res.status(500).json({ message: err.message || "Server error" });
+        const clientErrorMessages = new Set([
+            "Invalid email format",
+            "Email is already in use",
+            "Old password is incorrect",
+            "Same old and new password provided.",
+            `${GOOGLE_AUTH_MSG} email.`,
+            `${GOOGLE_AUTH_MSG} password.`,
+        ]);
+        const statusCode = clientErrorMessages.has(err.message) ? 400 : 500;
+        res.status(statusCode).json({ message: err.message || SERVER_ERROR_MSG });
     }
 };
