ebe6e0a2467efebd258844edb51b9530aaf1d1bb
Patteinh <patteinh@gmail.com>
2023-11-05 05:38:30 +0100

    [UPDATE] Dropbox area

--- server/src/area/dropboxArea.js
+++ server/src/area/dropboxArea.js
@@ -1,113 +1,68 @@
-const { Dropbox } = require("dropbox");
-const User = require("../models/userModels");
+const { getDropboxToken } = require("../utils/token/servicesTokenUtils");
+const { updateIngredients } = require("../utils/ingredients/ingredientsHelper");
+const { processTriggerDataTotal } = require("../utils/area/areaValidation");
+const { fetchUploadFile, fetchFilesInFolder, fetchAllSharedLinks } = require("../utils/API/dropboxAPI");
 
-let fetch;
+exports.newFileInFolder = async function (areaEntry) {
+    try {
+        const folderPath = areaEntry.trigger.parameters[0].input === "/" ? "" : areaEntry.trigger.parameters[0].input;
+        const accessToken = await getDropboxToken(areaEntry.userId);
+        const folderList = await fetchFilesInFolder(accessToken, folderPath);
+        const filesInFolderList = folderList.filter((link) => link[".tag"] === "file");
+        if (!folderList) return await processTriggerDataTotal(areaEntry, "newFileInFolder", "", 0);
+        const recentFile = filesInFolderList[totalFiles - 1];
 
-(async () => {
-    const nodeFetch = await import("node-fetch");
-    fetch = nodeFetch.default;
-})();
-
-async function getDropboxClientForUser(userId) {
-    const user = await User.findById(userId);
-    if (user && user.connectServices && user.connectServices.get("dropbox")) {
-        const accessToken = user.connectServices.get("dropbox").access_token;
-        return new Dropbox({ accessToken: accessToken, fetch: fetch });
-    } else {
-        throw new Error("Failed to get Dropbox client for user.");
-    }
-}
-
-function updateOrPushIngredient(ingredients, ingredient) {
-    const index = ingredients.findIndex((item) => item.name === ingredient.name);
-    if (index !== -1) {
-        ingredients[index].value = ingredient.value;
-    } else {
-        ingredients.push(ingredient);
-    }
-}
-
-async function getAllSharedLinks(dbxClient) {
-    let allLinks = [];
-    let hasMore = true;
-    let cursor = null;
-
-    while (hasMore) {
-        let response;
-        if (cursor) {
-            response = await dbxClient.sharingListSharedLinks({ cursor: cursor });
-        } else {
-            response = await dbxClient.sharingListSharedLinks();
+        if (await processTriggerDataTotal(areaEntry, "newFileInFolder", recentFile.id, filesInFolderList.length)) {
+            updateIngredients(areaEntry, [
+                { name: "file_name", value: recentFile.name },
+                { name: "folder_path", value: folderPath === "" ? "/" : folderPath },
+            ]);
+            return true;
         }
-        allLinks = allLinks.concat(response.result.links);
-        hasMore = response.result.has_more;
-        cursor = response.result.cursor;
-    }
-
-    return allLinks;
-}
-
-async function processTriggerData(areaEntry, key, value) {
-    if (!areaEntry.trigger.data) {
-        areaEntry.trigger.data = { key, value };
-        await areaEntry.save();
         return false;
-    } else if (areaEntry.trigger.data.value !== value) {
-        areaEntry.trigger.data = { key, value };
-        await areaEntry.save();
-        return true;
+    } catch (error) {
+        console.error("Failed in newFileInFolder function:", error);
+        throw error;
     }
-    return false;
-}
+};
 
-async function newFileInFolder(areaEntry) {
-    const dbxClient = await getDropboxClientForUser(areaEntry.userId);
-    const folderPath = areaEntry.trigger.parameters[0].input;
-    const filesListResponse = await dbxClient.filesListFolder({ path: folderPath });
-    const entries = filesListResponse.result.entries;
-    if (entries.length === 0) {
-        await processTriggerData(areaEntry, "newFileInFolder", "");
+exports.newSharedFileLink = async function (areaEntry) {
+    try {
+        const accessToken = await getDropboxToken(areaEntry.userId);
+        const allLinks = await fetchAllSharedLinks(accessToken);
+        const fileLinks = allLinks.filter((link) => link[".tag"] === "file");
+        console.log(JSON.stringify(fileLinks, null, 2));
+        if (fileLinks.length === 0) return await processTriggerDataTotal(areaEntry, "newSharedFileLink", "", 0);
+        const recentFileLink = fileLinks[0];
+
+        if (await processTriggerDataTotal(areaEntry, "newSharedFileLink", recentFileLink.id, fileLinks.length)) {
+            updateIngredients(areaEntry, [
+                { name: "file_url", value: recentFileLink.url },
+                { name: "file_name", value: recentFileLink.name },
+            ]);
+            return true;
+        }
         return false;
+    } catch (error) {
+        console.error("Failed in newSharedFileLink function:", error);
+        throw error;
     }
-    const recentFile = entries[0];
-    updateOrPushIngredient(areaEntry.trigger.ingredients, { name: "file_name", value: recentFile.name });
-    updateOrPushIngredient(areaEntry.trigger.ingredients, { name: "folder_path", value: folderPath });
-    return await processTriggerData(areaEntry, "newFileInFolder", recentFile.id);
-}
+};
 
-async function newSharedFileLink(areaEntry) {
-    const dbxClient = await getDropboxClientForUser(areaEntry.userId);
-    const allLinks = await getAllSharedLinks(dbxClient);
-    if (!allLinks || allLinks.length === 0) {
-        await processTriggerData(areaEntry, "newSharedFileLink", "");
-        return false;
+exports.uploadFileFromURL = async function (userId, fileUrl, fileName, filePath) {
+    try {
+        const accessToken = await getDropboxToken(userId);
+        const response = await fetch(fileUrl);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch file from URL: ${response.statusText}`);
+        }
+        const buffer = await response.arrayBuffer();
+        const fileData = Buffer.from(buffer);
+        const uploadResult = await fetchUploadFile(accessToken, fileData, fileName, filePath);
+        return uploadResult;
+    } catch (error) {
+        console.error("Error in uploadFileFromURL function:", error);
+        throw error;
     }
-    const fileLinks = allLinks.filter((link) => link[".tag"] === "file");
-    const recentFileLink = fileLinks[0];
-    updateOrPushIngredient(areaEntry.trigger.ingredients, { name: "file_url", value: recentFileLink.url });
-    updateOrPushIngredient(areaEntry.trigger.ingredients, { name: "file_name", value: recentFileLink.name });
-    return await processTriggerData(areaEntry, "newSharedFileLink", recentFileLink.id);
-}
-
-async function moveFileOrFolder(userId, originalPath, destinationPath) {
-    if (destinationPath == "/")
-        destinationPath = "";
-    const dbxClient = await getDropboxClientForUser(userId);
-    await dbxClient.filesMoveV2({ from_path: originalPath, to_path: destinationPath });
-}
-
-async function addFileFromURL(userId, fileUrl, fileName, dropboxFolderPath) {
-    if (dropboxFolderPath == "/")
-        dropboxFolderPath = "";
-    const dbxClient = await getDropboxClientForUser(userId);
-    const response = await fetch(fileUrl);
-    const fileData = await response.buffer();
-    await dbxClient.filesUpload({ path: `${dropboxFolderPath}/${fileName}`, contents: fileData });
-}
-
-module.exports = {
-    newFileInFolder,
-    newSharedFileLink,
-    moveFileOrFolder,
-    addFileFromURL,
 };
