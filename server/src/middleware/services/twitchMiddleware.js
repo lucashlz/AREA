b11b7bf991989ee0e1899a4d5e383c224b330399
Patteinh <patteinh@gmail.com>
2023-11-05 05:43:18 +0100

    [ADD] twitch middleware

---
+++ server/src/middleware/services/twitchMiddleware.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+async function twitchOAuthMiddleware(req, res, next) {
+    const code = req.query.code;
+    const redirectUri = "http://localhost:8080/connect/twitch/callback";
+    const clientId = process.env.TWITCH_CLIENT_ID;
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+    try {
+        const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
+            params: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                code: code,
+                redirect_uri: redirectUri,
+                grant_type: 'authorization_code'
+            }
+        });
+        const { access_token, refresh_token, expires_in } = response.data;
+        const profileResponse = await axios.get('https://api.twitch.tv/helix/users', {
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+                'Client-ID': clientId
+            }
+        });
+        const profile = profileResponse.data.data[0];
+        req.twitchData = {
+            accessToken: access_token,
+            refreshToken: refresh_token,
+            expiresIn: expires_in,
+            profile: profile,
+        };
+        next();
+    } catch (error) {
+        console.error("Error during token exchange:", error);
+        return res.status(500).json({ status: "error", message: "Internal server error." });
+    }
+}
+
+module.exports = twitchOAuthMiddleware;
