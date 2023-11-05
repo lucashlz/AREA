f713ea68054e115683f589acb97577a93e8ac3fb
Patteinh <patteinh@gmail.com>
2023-11-05 05:40:41 +0100

    [UPDATE] aboutController

--- server/src/controllers/aboutController.js
+++ server/src/controllers/aboutController.js
@@ -1,39 +1,19 @@
-const AREAS = require("../core/areaServices");
+const { AREAS } = require("../core/areaServices");
+const { formatService } = require("../utils/about/serviceFormatters");
 
 exports.getInfo = (req, res) => {
     try {
         const services = Object.keys(AREAS)
-            .map((areaKey) => {
-                const area = AREAS[areaKey];
-                if (!area.triggers || !area.actions) {
-                    console.error(`Area ${areaKey} is missing either triggers or actions`);
-                    return null;
-                }
-
-                return {
-                    name: areaKey,
-                    color: area.color,
-                    triggers: area.triggers.map((trigger) => ({
-                        name: trigger.name,
-                        description: trigger.description,
-                        parameters: trigger.parameters,
-                        ingredients: trigger.ingredients || [],
-                    })),
-                    actions: area.actions.map((action) => ({
-                        name: action.name,
-                        description: action.description,
-                        parameters: action.parameters,
-                    })),
-                };
-            })
+            .map((areaKey) => formatService(areaKey, AREAS[areaKey]))
             .filter(Boolean);
+
         res.status(200).json({
             client: {
                 host: req.ip,
             },
             server: {
                 current_time: Math.floor(Date.now() / 1000),
-                services: services,
+                services,
             },
         });
     } catch (error) {
