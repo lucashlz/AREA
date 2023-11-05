ab7ffb8ed6e8a3fb81316cc07b93ca366de0c177
Patteinh <patteinh@gmail.com>
2023-11-05 05:46:05 +0100

    [ADD]  index.js for utils folder

---
+++ server/src/utils/about/index.js
@@ -0,0 +1,9 @@
+const { formatService } = require("./serviceFormatters");
+const { formatTrigger } = require("./triggerFormatters");
+const { formatAction } = require("./actionFormatters");
+
+module.exports = {
+    formatService,
+    formatTrigger,
+    formatAction,
+};
